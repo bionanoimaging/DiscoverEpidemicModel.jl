c1aa87c19d08f5e1076efe02108832b46dec7b1d
Documenter.jl <documenter@juliadocs.github.io>
2022-12-05 13:40:13 +0000

    build based on 6720786

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiscoverEpidemicModel","category":"page"},{"location":"#DiscoverEpidemicModel","page":"Home","title":"DiscoverEpidemicModel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DiscoverEpidemicModel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiscoverEpidemicModel]","category":"page"}]
+}
