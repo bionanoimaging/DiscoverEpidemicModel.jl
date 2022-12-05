725abf048d6e08e18607a5bcbe78a2f47a92c617
Felix Wechsler (he/him) <fxw+git@mailbox.org>
2022-12-05 14:57:37 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,20 +6,13 @@ DocMeta.setdocmeta!(DiscoverEpidemicModel, :DocTestSetup, :(using DiscoverEpidem
 makedocs(;
     modules=[DiscoverEpidemicModel],
     authors="Morteza Babazadeh, Rainer Heintzmann, René Lachmann",
-    repo="https://github.com/bionanoimaging/DiscoverEpidemicModel.jl/blob/{commit}{path}#{line}",
     sitename="DiscoverEpidemicModel.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://bionanoimaging.github.io/DiscoverEpidemicModel.jl",
-        edit_link="main",
-        assets=String[],
-    ),
     pages=[
         "Home" => "index.md",
     ],
 )
 
 deploydocs(;
-    repo="github.com/bionanoimaging/DiscoverEpidemicModel.jl",
-    devbranch="main",
+    repo="github.com/bionanoimaging/DiscoverEpidemicModel.jl.git",
+    devbranch="master",
 )
